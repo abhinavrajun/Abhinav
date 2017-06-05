cb22b65eda2ce21d5b3f59e3df6fd027af22d4b8
abhinavrajun <abhinavrajun@gmail.com>
2017-06-05 16:00:44 +0530

    stack

--- Hello1.java
+++ Hello1.java
@@ -1,8 +1,35 @@
 class Hello
 {
+int top=-1;
+ int a[]=new int[10];
+ void push()
+ {
+   top++;
+   if(top>10)
+   {
+     System.out.println("is full");
+     return;
+   }
+     Scanner s=new Scanner(System.in);
+     System.out.println("enter the no.: ");
+      int no=s.nextInt();
+
+   a[top]=no;
+ }
  public static void main(String[] args)
   {
-		System.out.println("HI");
+    Scanner sc=new Scanner(System.in);
+    do{
+    System.out.println("enter 1 to push");
+      int i=sc.nextint();
+      Switch(i)
+      {
+          case 1: push();
+                   break;
+          default: break;
   }
-  
+    System.out.println("press 0 to end");
+  int j=sc.nextInt();    }while(j!=0);
+  }
+
 }
