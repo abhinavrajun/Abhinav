2ede532614dd101821e85dae995dfc9b485ddbd7
abhinavrajun <abhinavrajun@gmail.com>
2017-06-06 11:02:04 +0530

    made changes

--- Hello1.java
+++ Hello1.java
@@ -14,7 +14,7 @@ int top=-1;
  void push()
  {
    top++;
-   if(top>10)
+   if(top>4)
    {
      System.out.println("\nis full\n");
      return;
@@ -22,14 +22,14 @@ int top=-1;
      Scanner s=new Scanner(System.in);
      System.out.println("enter the no.: ");
       int no=s.nextInt();
-     
+
    a[top]=no;
  }
  void pop(){
      if(top==-1){
          System.out.println("\nthe stack is empty\n");
      }
-     else{ 
+     else{
      top--;
  }
  }
@@ -39,20 +39,20 @@ int top=-1;
                  System.out.println("\n"+a[j]);
                  j++;
 
-        
+
     }
 }
-     
+
  public static void main(String[] args)
   {int j;
   Hello1 od=new Hello1();
     Scanner sc=new Scanner(System.in);
     int i;
     do{
-    System.out.println("1)Enter '1' to push\n2)Enter '2' to pop\n3)Enter '3' to display\n4)Enter '4' to exit\n");  
+    System.out.println("1)Enter '1' to push\n2)Enter '2' to pop\n3)Enter '3' to display\n4)Enter '4' to exit\n");
       i=sc.nextInt();
       switch(i){
-          case 1: 
+          case 1:
               od.push();
                    break;
           case 2:
@@ -61,10 +61,10 @@ int top=-1;
           case 3:
               od.display();
               break;
-          default: break;         
-  }    
-   
+          default: break;
+  }
+
       }while(i!=4);
   }
-  
+
 }
